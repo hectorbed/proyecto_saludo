534948ee5952481e365d74cdc6b59a830c1da16b
Hector Bedoya <hectorbed@Hectors-MacBook-Pro.local>
2024-08-31 11:09:20 -0500

    Prueba

--- src/main/java/com/diplomado/proyecto_saludo/SaludarController.java
+++ src/main/java/com/diplomado/proyecto_saludo/SaludarController.java
@@ -9,6 +9,6 @@ public class SaludarController {
 
     @GetMapping("/saludar")
     public ResponseEntity<?> saludar(){
-        return ResponseEntity.ok().body("Hola con ArgoCD y Helm!");
+        return ResponseEntity.ok().body("Hola, ArgoCD y Helm!");
     }
 }
